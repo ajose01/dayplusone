00dc8b803fe00f74bcc1f6cb8a5eaef4f4dc870e
Angel Jose <ajose01@gmail.com>
2014-10-28 00:27:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dayplusone::Application.config.secret_key_base = 'e96515a107d51d96ac1fe39b808863f0db454f4b51b8ff3c8b04b60ec221fce76d8bd6bb67c627656af999f3304e6ee16acce57b8ae0c98655be6908ab22bf4d'
