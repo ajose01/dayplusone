d829936d337e8d115c842ec5d093ed16febd807c
Angel Jose <ajose01@gmail.com>
2014-10-29 07:16:04 +0000

    Added user tracking logic to User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def on_track?
+  	self.updated_at.to_date > 2.days.ago
+  end
+
+  def on_time?
+  	self.updated_at.to_date == Date.current
+  end
+
+
+
 end
